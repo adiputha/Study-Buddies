d854101fac3d67c6daf1ff8a21d1e9158990f6b7
adiputha <adithyaperera456@gmail.com>
2024-10-24 18:39:58 +0530

    search

--- src/app/(dashboard)/list/students/[id]/page.tsx
+++ src/app/(dashboard)/list/students/[id]/page.tsx
@@ -64,8 +64,8 @@ const SingleStudentPage = () => {
                 <span className="text-sm text-gray-400">Attendance</span>
               </div>
             </div>
-             {/* CARD */}
-             <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+            {/* CARD */}
+            <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
               <Image
                 src="/singleBranch.png"
                 alt=""
@@ -78,8 +78,8 @@ const SingleStudentPage = () => {
                 <span className="text-sm text-gray-400">Grade</span>
               </div>
             </div>
-             {/* CARD */}
-             <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+            {/* CARD */}
+            <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
               <Image
                 src="/singleLesson.png"
                 alt=""
@@ -92,8 +92,8 @@ const SingleStudentPage = () => {
                 <span className="text-sm text-gray-400">Lessons</span>
               </div>
             </div>
-             {/* CARD */}
-             <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+            {/* CARD */}
+            <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
               <Image
                 src="/singleClass.png"
                 alt=""
@@ -110,24 +110,34 @@ const SingleStudentPage = () => {
         </div>
         {/* BOTTOM */}
         <div className="mt-4 bg-white rounded-md p-4 h-[800px]">
-           <h1> Student&apos;s Schedule</h1>
-           <BigCalendar/>
-            </div>
+          <h1> Student&apos;s Schedule</h1>
+          <BigCalendar />
+        </div>
       </div>
       {/*RIGHT*/}
       <div className="w-full xl:w-1/3 flex flex-col gap-4">
-      <div className="bg-white p-4 rounded-md">
-        <h1 className="text-xl font-semibold">Shortcuts</h1>
-        <div className="mt-4 flex gap-4 flex-wrap text-xs text-gray-500">
-            <Link href="/" className="p-3 rounded-md bg-skyLight">Student&apos;s Lessons</Link>
-            <Link href="/" className="p-3 rounded-md bg-purpleLight">Student&apos;s Teachers</Link>
-            <Link href="/" className="p-3 rounded-md bg-pink-50">Student&apos;s Exams</Link>
-            <Link href="/" className="p-3 rounded-md bg-skyLight">Student&apos;s Assignments</Link>
-            <Link href="/" className="p-3 rounded-md bg-yellowLight">Student&apos;s Results</Link>
+        <div className="bg-white p-4 rounded-md">
+          <h1 className="text-xl font-semibold">Shortcuts</h1>
+          <div className="mt-4 flex gap-4 flex-wrap text-xs text-gray-500">
+            <Link href="/" className="p-3 rounded-md bg-skyLight">
+              Student&apos;s Lessons
+            </Link>
+            <Link href={`/list/teachers?classId=${2}`} className="p-3 rounded-md bg-purpleLight">
+              Student&apos;s Teachers
+            </Link>
+            <Link href="/" className="p-3 rounded-md bg-pink-50">
+              Student&apos;s Exams
+            </Link>
+            <Link href="/" className="p-3 rounded-md bg-skyLight">
+              Student&apos;s Assignments
+            </Link>
+            <Link href="/" className="p-3 rounded-md bg-yellowLight">
+              Student&apos;s Results
+            </Link>
+          </div>
         </div>
-      </div>
-      <Performance/>
-        <Announcement/>      
+        <Performance />
+        <Announcement />
       </div>
     </div>
   );
--- src/app/(dashboard)/list/students/page.tsx
+++ src/app/(dashboard)/list/students/page.tsx
@@ -2,14 +2,22 @@ import FormModel from "@/components/FormModel";
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
-import { role, teachersData } from "@/lib/data";
-import prisma from "@/lib/prisma";
-import { Class, Subject, Teacher } from "@prisma/client";
+import { role, studentsData, teachersData } from "@/lib/data";
 import { access } from "fs";
 import Image from "next/image";
 import Link from "next/link";
 
-type TeacherList = Teacher & { subjects: Subject[] } & { classes: Class[] };
+type Student = {
+  id: number;
+  studentId: string;
+  name: string;
+  email?: string;
+  photo: string;
+  phone?: string;
+  grade: number;
+  class: string;
+  address: string;
+};
 
 const columns = [
   {
@@ -17,20 +25,16 @@ const columns = [
     accessor: "info",
   },
   {
-    header: "Teacher ID",
-    accessor: "teacherId",
+    header: "Student ID",
+    accessor: "studentId",
     className: "hidden md:table-cell",
   },
   {
-    header: "Subjects",
-    accessor: "subjects",
+    header: "Grade",
+    accessor: "grade",
     className: "hidden md:table-cell",
   },
-  {
-    header: "Classes",
-    accessor: "classes",
-    className: "hidden md:table-cell",
-  },
+
   {
     header: "Phone",
     accessor: "phone",
@@ -47,14 +51,14 @@ const columns = [
   },
 ];
 
-const renderRow = (item: TeacherList) => (
+const renderRow = (item: Student) => (
   <tr
     key={item.id}
     className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-purpleLight"
   >
     <td className="flex items-center gap-4 p-4">
       <Image
-        src={item.img || "/noAvatar.png"}
+        src={item.photo}
         alt={item.name}
         width={40}
         height={40}
@@ -62,21 +66,16 @@ const renderRow = (item: TeacherList) => (
       />
       <div className="flex flex-col">
         <h3 className="font-semibold">{item.name}</h3>
-        <p className="text-xs text-gray-500">{item?.email}</p>
+        <p className="text-xs text-gray-500">{item?.class}</p>
       </div>
     </td>
-    <td className="hidden md:table-cell">{item.username}</td>
-    <td className="hidden md:table-cell">
-      {item.subjects.map((subject) => subject.name).join(", ")}
-    </td>
-    <td className="hidden md:table-cell">
-      {item.classes.map((classItem) => classItem.name).join(", ")}
-    </td>
+    <td className="hidden md:table-cell">{item.studentId}</td>
+    <td className="hidden md:table-cell">{item.grade}</td>
     <td className="hidden md:table-cell">{item.phone}</td>
     <td className="hidden md:table-cell">{item.address}</td>
     <td>
       <div className="flex items-center gap-2">
-        <Link href={`/list/teachers/${item.id}`}>
+        <Link href={`/list/students/${item.id}`}>
           <button className="w-7 h-7 flex items-center justify-center rounded-full bg-sky">
             <Image src="/view.png" alt="" width={16} height={16} />
           </button>
@@ -86,37 +85,21 @@ const renderRow = (item: TeacherList) => (
           //     <Image src="/delete.png" alt="" width={16} height={16}/>
           // </button>
 
-          <>
-            <FormModel table="subject" type="update" data={item} />
-            <FormModel table="subject" type="delete" id={item.id} />
-          </>
+          <FormModel table="student" type="delete" id={item.id} />
         )}
       </div>
     </td>
   </tr>
 );
 
-const TeacherList = async ({
-  searchParams,
-}: {
-  searchParams: { [key: string]: string } | undefined;
-}) => {
-  console.log(searchParams);
 
-  const data = await prisma.teacher.findMany({
-    include: {
-      subjects: true,
-      classes: true,
-    },
-    take: 10,
-  });
-//   console.log(data);
+const StudentList = async () => {
 
   return (
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
       {/*TOP*/}
       <div className="flex items-center justify-between">
-        <h1 className="hidden md:block text-lg font-semibold">All Teachers</h1>
+        <h1 className="hidden md:block text-lg font-semibold">All Students</h1>
         <div className="flex flex-col md:flex-row items-center gap-4  w-full md:w-auto">
           <TableSearch />
           <div className="flex items-center gap-4 self-end">
@@ -130,17 +113,18 @@ const TeacherList = async ({
               //     <button className="w-8 h-8 flex items-center justify-center rounded-full bg-yellow">
               //     <Image src="/plus.png" alt="" width={14} height={14}/>
               // </button>
-              <FormModel table="teacher" type="create" />
+
+              <FormModel table="student" type="create" />
             )}
           </div>
         </div>
       </div>
       {/*LIST*/}
-      <Table columns={columns} renderRow={renderRow} data={data} />
+      <Table columns={columns} renderRow={renderRow} data={studentsData} />
       {/*PAGINATION*/}
       <Pagination />
     </div>
   );
 };
 
-export default TeacherList;
+export default StudentList;
